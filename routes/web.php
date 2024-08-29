d1bf4be57db7d937614347e1b6f551bf7bf3c136
Eslam <eslamamr537@gmail.com>
2024-08-29 20:30:37 +0300

    admin rtl ltr

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use Illuminate\Support\Facades\App;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){ //...
+
+        Route::get('/', function () {
+            return view('index');
+        });
+        Route::get('/{page}', AdminController::class);
+    });
+
+
+
