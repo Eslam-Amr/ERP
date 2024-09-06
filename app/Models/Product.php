39aa0d49e5c6cd2b5d710e4aae2927b8c7b226d4
zeyad814 <ahmedomar1423@gmail.com>
2024-09-06 19:48:57 +0300

    model-commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,29 +6,31 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
+
 class Product extends Model implements TranslatableContract
 {
     use  HasFactory, Translatable;
 
     public $translatedAttributes = ['name', 'model', 'description'];
     // protected $fillable = ['author'];
-    public function infos(){
+    public function infos()
+    {
         return $this->hasMany(ProductInfo::class);
-
     }
-//     protected $fillable=[
-// 'name',
-// 'model',
-// 'weight',
-// 'price',
-// 'discount',
-// 'description',
-// 'final_quantity',
-//     ];
-    protected $fillable=[
-'weight',
-'price',
-'discount',
-'final_quantity',
+    //     protected $fillable=[
+    // 'name',
+    // 'model',
+    // 'weight',
+    // 'price',
+    // 'discount',
+    // 'description',
+    // 'final_quantity',
+    //     ];
+    protected $fillable = [
+        'weight',
+        'price',
+        'discount',
+        'final_quantity',
+        '7mada'
     ];
 }
